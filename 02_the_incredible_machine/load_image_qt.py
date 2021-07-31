9ebf9dec87c22d32a996d176b1fcdde9ad99bd52
tiancheng2000 <3139658+tiancheng2000@users.noreply.github.com>
2021-07-31 17:18:18 +0800

    #day1

---
+++ 02_the_incredible_machine/load_image_qt.py
@@ -0,0 +1,7 @@
+
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget,
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    pixmap = QtGui.QPixmap("mouse.png")
