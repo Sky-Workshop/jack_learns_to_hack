fb7c3c928c8518503cdebcbb5bf8bfa980e8724a
tiancheng2000 <3139658+tiancheng2000@users.noreply.github.com>
2021-08-01 16:20:05 +0800

    #day1_step2.3: #trick: how to avoid printing 3*2, 4*2, 4*3 ...? breakpoint will be helpful!

--- 01_times_table/times_table.py
+++ 01_times_table/times_table.py
@@ -1,5 +1,5 @@
-#day1_step2.2: omit 1*n and n*1, and add a group seprator
-for i in range(1,9):
-	for j in range(1,9):
+#day1_step2.3: #trick: how to avoid printing 3*2, 4*2, 4*3 ...? breakpoint will be helpful!
+for i in range(1, 9):
+	for j in range(i, 9):
 		print(i+1, "*", j+1, "=", (i+1)*(j+1))
 	print("---------")
