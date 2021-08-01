6ab888cfdf6003e1ad795edd1010deb75a64cdab
tiancheng2000 <3139658+tiancheng2000@users.noreply.github.com>
2021-08-01 16:17:05 +0800

    #day1_step2.2: omit 1*n and n*1, and add a group seprator

--- 01_times_table/times_table.py
+++ 01_times_table/times_table.py
@@ -1,3 +1,5 @@
-for i in range(0,9):
-	for j in range(0,9):
+#day1_step2.2: omit 1*n and n*1, and add a group seprator
+for i in range(1,9):
+	for j in range(1,9):
 		print(i+1, "*", j+1, "=", (i+1)*(j+1))
+	print("---------")
